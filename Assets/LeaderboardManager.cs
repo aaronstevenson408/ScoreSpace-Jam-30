5b1658c0225f4301538ee81ba996d4793a82571b
04c-amaya <04c.amaya@gmail.com>
2024-06-21 10:36:29 -0400

    Started to implement Loolocker into unity
    Got Lootlocker connected to untiy
    created guest login system for players
    created leaderboard

---
+++ Assets/LeaderboardManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LootLocker.Requests;
+
+public class LeaderboardManager : MonoBehaviour
+{
+     public string leaderBoardKey = "test_score";
+
+
+    private void Update()
+    {
+    }
+    private void Start()
+    {
+        
+    }
+    public void SubmitScore(int _score)
+    {
+        string playerID = PlayerPrefs.GetString("PlayerID");
+
+        LootLockerSDKManager.SubmitScore(playerID, _score, leaderBoardKey, (response) =>
+        {
+            if (!response.success)
+            {
+                Debug.Log("Could not submit score!");
+                Debug.Log(response.errorData.ToString());
+                return;
+            }
+            Debug.Log("Successfully submitted score!");
+        });
+    }
+    }
