155e74c2a0374b11e1ce2a1d882fe5645655c493
xXClawtheWolfXx <hayleytheblossomkitty@gmail.com>
2024-06-22 13:51:58 -0400

    End state, timer and soap pickup added
    -moved timer into WorldManager
    --created game states,
    -there is now a end state which happens when the bubble is pop
    --crashing into soap pickups makes them disappear and adds time

--- Assets/Scripts/Player/Bubble.cs
+++ Assets/Scripts/Player/Bubble.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+    //there is only one bubble in the scene
+    private static Bubble instance;
+    public static Bubble Instance { get { return instance; } }
+
+    [SerializeField] private bool canGrow;
     [SerializeField] private float minSize = 1;
     [SerializeField] private float maxSize = 5;
+    [SerializeField] private float growSpeed = .02f;
     [SerializeField] private Transform bubbleSprite;
+
+    float timer;
+
+    void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
-        // bubbleSprite.localScale = new Vector3(minSize, minSize, minSize);
+        if (canGrow)
+            bubbleSprite.localScale = new Vector3(minSize, minSize, minSize);
     }
 
     // Update is called once per frame
     void Update()
     {
         //bubble grows in size based on time
-        //float sizeAmt = GameTime.Instance.GetTime / maxSize;
-        //float sizeAmt = time + minSize;
-        // sizeAmt = Mathf.Clamp(sizeAmt, minSize, maxSize);
-        // bubbleSprite.localScale = Vector3.one * sizeAmt;
+        if (canGrow) Grow();
+    }
 
-
+    void Grow()
+    {
+        timer += Time.deltaTime;
+        float sizeAmt = timer / maxSize * growSpeed;
+        sizeAmt = Mathf.Clamp(sizeAmt, minSize, maxSize);
+        bubbleSprite.localScale = Vector3.one * sizeAmt;
+        //Debug.Log(bubbleSprite.localScale);
 
     }
 
@@ -31,6 +49,7 @@ public class Bubble : MonoBehaviour
     {
         bubbleSprite.gameObject.SetActive(false);
         bubbleSprite.localScale = new Vector3(minSize, minSize, minSize);
+        WorldManager.Instance.End();
         //lose
     }
 }
